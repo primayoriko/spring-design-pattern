ce835e7e5cace276414cd627bf78843143040e4d
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-01-28 10:29:20 +0700

    step 1

--- src/main/java/my/primayoriko/springbasic/SpringBasicApplication.java
+++ src/main/java/my/primayoriko/springbasic/SpringBasicApplication.java
@@ -2,12 +2,36 @@ package my.primayoriko.springbasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+// import java.lang.module.Configuration;
 
 @SpringBootApplication
 public class SpringBasicApplication {
 
+	@SpringBootApplication
+	public static class Configuration{
+
+		@Bean
+		public Contoh contoh(){
+			return new Contoh();
+		}
+	}
+
+	public static class Contoh {
+
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBasicApplication.class, args);
+		ApplicationContext context = SpringApplication.run(Configuration.class);
+
+		Contoh contoh1 = context.getBean(Contoh.class);
+		Contoh contoh2 = context.getBean(Contoh.class);
+
+		System.out.println(contoh1 == contoh2);
+
+//		SpringApplication.run(SpringBasicApplication.class, args);
 	}
 
 }
