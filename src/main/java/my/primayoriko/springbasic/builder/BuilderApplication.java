a63da0f94f46385dc922b379a8d74f1c137e7591
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-01-28 11:11:31 +0700

    step 3: add builder

---
+++ src/main/java/my/primayoriko/springbasic/builder/BuilderApplication.java
@@ -0,0 +1,37 @@
+package my.primayoriko.springbasic.builder;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class BuilderApplication {
+
+    @Builder
+    @Data
+    @AllArgsConstructor
+    public static class Mahasiswa {
+        private String name;
+        private Integer id;
+        private Date tanggalLahir;
+
+        @Singular("hobi")
+        private List<String> hobi;
+    }
+
+    public static void main(String[] args) {
+
+        Mahasiswa mahasiswa = Mahasiswa.builder()
+                .id(123)
+                .name("uhuy")
+                .tanggalLahir(new Date())
+                .hobi("makan")
+                .hobi("tidur")
+                .hobi("rebahan")
+                .build();
+
+        System.out.println(mahasiswa);
+    }
+
+}
